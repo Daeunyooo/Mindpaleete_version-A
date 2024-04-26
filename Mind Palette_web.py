cd6ba99b33c86823d8927efbf039a43a622f86a3
Daeunyooo <110686967+Daeunyooo@users.noreply.github.com>
2024-04-26 11:57:49 -0400

    Update Mind Palette_web.py

--- Mind Palette_web.py
+++ Mind Palette_web.py
@@ -585,5 +585,5 @@ def home():
     """, latest_question=latest_question, progress_value=progress_value)
 
 if __name__ == '__main__':
-    app.secret_key = OPENAI_API_KEY
+    app.secret_key = os.environ['OPENAI_API_KEY']
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
